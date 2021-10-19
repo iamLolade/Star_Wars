c77a8cc95e744e47a5ea2d28ce1f8333d1ca8f4c
iamLolade <adeniyiotemade@gmail.com>
2021-10-19 18:13:41 +0100

    Created pop-up with bootstrap modal template

--- script/index.js
+++ script/index.js
@@ -22,6 +22,27 @@ for (let i = 0; i < dataArray.length; i++) {
           </div>
       </div>
     </div>
+
+
+    <!-- Modal -->
+    <div class="my-3 modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" id="exampleModalLabel"><h4 class="card-title">I'm ${dataArray[i].name}</h4></h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <h5 class="modal-title" id="exampleModalLabel"><h5 class="card-title">Gender: ${dataArray[i].gender.toUpperCase()}</h5></h5>
+            <h5 class="modal-title" id="exampleModalLabel"><h5 class="card-title">Height: ${dataArray[i].height}</h5></h5>
+            <img src="./image/img${i}.jpeg" class="modal-img img responsive" alt="starwars_characters">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
     `
 
 }
